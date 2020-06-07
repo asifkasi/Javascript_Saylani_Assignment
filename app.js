271f3407c114616b8c0f386bd0edc0d48d894177
asifkasi <60402339+asifkasi@users.noreply.github.com>
2020-06-07 16:46:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,551 @@
+//Q1:Write a script to greet your website visitor using JS alert box.
+//alert("Welcome");
+
+//2. Write a script to display following message on your web page:
+// alert("Javascript alert\n Error! enter a valid Password");
+
+//3. Write a script to display following message on your web page: (Hint : Use line break)
+// alert("Javascript alert\n\n Welcom to JS Land \nHappy coding!");
+
+// 4. Write a script to display following messages in sequence:
+// alert("Javascript alert\n\n Welcom to JS Land");
+// alert("Happy coding!");
+
+//5. Generate the following message through browser’s developer console:
+// alert("Javascript alert\n\n Hello.. I can run JS through my web broswer's console");
+
+
+//6. Make use of alerts in your new/existing HTML & CSS project.
+//Done
+
+//7. Practice placement of <script></script> element in following sections of your project in exercise 6:
+//Done
+
+//              Chapter#2
+
+//1. Declare a variable called username.
+// var username;
+
+//2. Declare a variable called myName & assign to it a string that represents your Full Name.
+// var myName ="Asif Kasi";
+
+//3
+// var message = "Hello Wolrd";
+// alert(message);
+
+//4
+
+// var studentname= "Asif Kasi";
+// var studentid = "WM7431"
+// alert(studentname);
+// alert(studentid);
+
+//5
+
+// var pizza = "pizza\npizz\npiz\npi\np"
+// alert(pizza)
+
+//6
+// var email = "asifkasi37@gmail.com"
+// alert("My email Address is "+email)
+
+//7
+// var book = "A smarter way to learn JavaScript"
+// alert("i am tring to learn from "+book)
+
+//8
+// document.write("Yeah! I can write HTML code in Javascript");
+
+//9
+// var graph = "“▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(graph)
+
+
+//          Chapter#3
+
+//1
+// var age = 22;
+// alert("I am "+age+" years old")
+
+//2
+
+//3
+// birthyear = 1996;
+// document.write("My birth year is "+birthyear+"<br>");
+// document.write("Data type os my birthyear is number")
+
+//4
+// var name = prompt("Enter your name");
+// var product = prompt("Enter your product title");
+// var quantity = prompt("Enter quantity");
+
+// document.write(name+" orders "+quantity+" "+product+"(s) on XYZ Clothing store")
+
+//              Chapter#4
+
+//1
+// var var1 = 10,var2 = 5,var3 = 50;
+
+//2
+// var name = "",Declare_ = "",_D="",$d="",d1="";
+// var 1d="",/d="", function="",Prompt="",
+
+//3
+
+// document.write("<h1>Rules for naming JS variables</h1>")
+// document.write("Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable <br>")
+// document.write("Variable names are case _________<br>")
+// document.write("Variable names should not be JS _________<br>")
+
+
+
+//ch#5
+
+
+//q1
+
+// var var1 = +prompt("Enter First number")
+// var var2 = +prompt("Enter second number")
+
+// var3 = var1+var2
+// document.write("the sum of "+var1+" and "+var2+" is ",var3)
+
+//q2
+// var var1 = +prompt("Enter First number")
+// var var2 = +prompt("Enter second number")
+
+// var3 = var1+var2
+// var4 = var1-var2
+// var5 = var1*var2
+// var6 = var1/var2
+// var3 = var1%var2
+
+// document.write("the sum of "+var1+" and "+var2+" is ",var3)
+// document.write("the sub of "+var1+" and "+var2+" is ",var4)
+// document.write("the mul of "+var1+" and "+var2+" is ",var5)
+// document.write("the div of "+var1+" and "+var2+" is ",var6)
+// document.write("the mod of "+var1+" and "+var2+" is ",var7)
+
+
+//q3
+
+// var va = 5;
+// document.write("Initail value is ",va,"<br>")
+// ++va
+// document.write("value after increment is ",va,"<br>")
+// va=va+7
+// document.write("value is ",va,"<br>")
+// --va
+// document.write("value after decrement is ",va,"<br>")
+// va = va%3
+// document.write("value is ",va,"<br>")
+
+
+//Q4
+
+// var movie = +prompt("Enter the prize of movie")
+// document.write("total cost of 5 movies tickets are ",movie*5," PKR")
+
+
+//Q5
+// var no = +prompt("Enter the number of table")
+// document.write("Table of ",no,"<br>")
+// for (let index = 1; index < 11; index++) {
+//     document.write(no+" X ",index," = ",no*index,"<br>")
+
+// }
+
+//Q6
+// var tempC = 25
+// var tempF = 70
+
+// var ConF = (tempC *9/5)+32
+// var ConC = (tempF-32)*5/9
+
+// document.write(tempC,"C is ",ConF,"F <br >")
+// document.write(tempF,"F is ",ConC,"F")
+
+//Q7
+// document.write("<H1>SHOPPING CART</H1>")
+// var p1 = 650
+// var Q1  = 3
+// var P2 = 100
+// var Q2 = 7
+// var shipPr = 100
+// total = p1*Q1+P2*Q2+100
+// document.write("Price of Item1 is ",p1)
+// document.write("<br>Quantity of Item1 is ",Q1)
+
+// document.write("<br>Price of Item2 is ",P2)
+// document.write("<br>Quantity of Item2 is ",Q2)
+
+
+// document.write("<br>Total cost of your order is ",total)
+
+//Q8
+
+// document.write("<H1>marks Sheet</H1>")
+// var total = +prompt("Enter total marks")
+// var obtain = +prompt("Enter Obtain marks")
+// var per = obtain/total *100
+// document.write("Total marks: ",total)
+// document.write("<br>Obtain marks: ",obtain)
+// document.write("<br>Percentage marks: ",per,"%")
+
+//Q9
+// var total = 10*104.80 + 25*28
+// document.write("<H1>Currency in PKR</H1>")
+// document.write("total Currency in PKR: ",total)
+
+//Q10
+
+// var num = 10
+// var total = ((num +5)*10)/2
+
+//Q11
+// var currentyear = 2020
+// var birthyear = 1996
+// document.write("Your age is ",currentyear-birthyear)
+
+//Q12
+// var r =20
+// var cir = 2*3.142*r
+// var area = 3.142*(r*r)
+// document.write("Radius of Circle: ",r)
+// document.write("<br>Circumference of Circle: ",cir)
+// document.write("<br>Area of Circle: ",area)
+
+
+//Q13
+// document.write("<H1>The Lifetime Supply Calculator:</H1>")
+// var name = prompt("Favourite snake")
+// var Cage = +prompt("Current age")
+// var Mage = +prompt("Maximum age")
+// var finish = +prompt("Amount of snake per day")
+
+// var packet = ((Mage - Cage) *365)/finish
+// document.write("<br>Favourite snake:",name)
+// document.write("<br>Current Age:",Cage)
+// document.write("<br>Finihed Age:",Mage)
+// document.write("<br>Amount of snake per day:",finish)
+// document.write("<br> Yu will need ",packet," ",name," to you last untill old age",Mage)
+
+
+
+//      Ch#6-9
+
+//Q1
+
+// var a = +prompt("Enter the number")
+
+// document.write("<br>Result")
+// document.write("<br>The Number is ",a)
+// document.write("<br>--------------------------------")
+
+// document.write("<br>the value of ++a is ",++a)
+// document.write("<br>Now the Number is ",a)
+
+// document.write("<br>the value of a++ is ",a++)
+// document.write("<br>Now the Number is ",a)
+
+// document.write("<br>the value of --a is ",--a)
+// document.write("<br>Now the Number is ",a)
+
+// document.write("<br>the value of a-- is ",a--)
+// document.write("<br>Now the Number is ",a)
+
+
+
+// //Q2
+// var a = 2, b = 1; 
+// var result = --a - --b + ++b + b--;
+
+// document.write("a is ",a)
+// document.write("b is ",b)
+// document.write("result is ",result)
+
+
+
+// document.write("--a is ",0)
+// document.write("--a - --b is ",0)
+// document.write("--a - --b + ++b; is ",-1)
+// document.write("--a - --b + ++b + b--; is ",-3)
+
+
+
+//Q3
+
+// var name = prompt("Enter the name")
+// alert("welcome!",name)
+
+//Q5
+
+// var tb = prompt("Enter the number for table")
+// if (tb=="") {
+// tb = 5
+    
+// }
+// else   {
+//     for (let index = 1; index < 11; index++) {
+//         document.write("<br>",tb," X ",index," = ",tb*index)
+//     }
+// }
+
+
+//Q6
+
+// var s1 = prompt("Enter Firt subject name")
+// var s2 = prompt("Enter Firt subject name")
+// var s3 = prompt("Enter Firt subject name")
+// var total = 100
+// var ob1 = prompt("Enter "+s1+" Obtain Marks")
+// var ob2 = prompt("Enter "+s2+" Obtain Marks")
+// var ob3= prompt("Enter "+s3+" Obtain Marks")
+
+// var per1 = ob1/total*100
+// var per2 = ob1/total*100
+// var per3 = ob1/total*100
+
+    
+//     document.write("<table>        <tr>          <td>Subject</td>          <td>Total</td>          <td>Marks Obtained</td>          <td>Marks Percentage</td>        </tr>        <tr>          <td>",s1,"</td>          <td>",Total,"</td>          <td>",ob1,"</td>          <td>",per1,"</td>        </tr>               <tr><td>",s2,"</td>            <td>",Total,"</td>        <td>",ob2,"</td><td>",per2,"</td></tr><tr><td>",s3,"</td><td>",Total,"</td><td>",ob3,"</td><td>",per3,"</td></tr></table>")
+
+
+
+//Q7
+
+//          Ch#12-14
+
+//Q1
+
+// var ch = +prompt("Enter the Character")
+// if (ch>0 && ch<10) {
+//     document.write("Input is number")
+
+// }else if (ch>=65 && ch<=90){
+//     document.write("Input is Uppercase")    
+// }else if (ch>=94 && ch<=121){
+//     document.write("Input is Lowercase")    
+
+// }
+
+//Q2
+
+// var num1 = +prompt("Enter the Number 1")
+// var num2 = +prompt("Enter the Number 2")
+
+
+// if (num1>num2) {
+//     document.write("Number 1 is greater than number 2")
+
+// }else if (num1<num2){
+//     document.write("Number 1 is less than number 2")
+
+// }else if (num1==num2){
+//     document.write("Both numbers are equal")    
+
+// }
+
+
+//Q3
+
+// var num2 = +prompt("Enter the Number 2")
+
+
+// if (num2>0) {
+//     document.write("Number is +ve")
+
+// }else if (num2<0){
+//     document.write("Number is -ve")
+
+
+// }else if (num2==0){
+//     document.write("Number is 0")
+
+
+// }
+
+
+//Q4
+// var vowel = "aeiou"
+// var ch = prompt("Enter Character")
+
+// ch = ch.toLowerCase()
+// var flag = 0
+// for (let index = 0; index < vowel.length; index++) {
+//     if (ch==vowel[index]) {
+//         document.write("True")
+//         flag =1
+//         break
+//     }
+
+// }
+
+// if (flag==0) {
+
+//         document.write("False")
+
+// }
+
+
+//Q5
+// var pass = "Khan0300"
+// var pass1 = prompt("Enter password")
+
+// if (pass1=="") {
+//     document.write("Enter Password")
+
+// }else if (pass1===pass){
+//     document.write("Password is incorrect")
+
+
+// }else{
+//     document.write("Password is incorrect")
+
+
+// }
+
+// 6. This if/else statement does not work. Try to fix it: 
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+//     greeting = "Good day";
+// }
+// else {
+//     greeting = "Good evening";
+// }
+
+
+//Q7
+
+// var time = +prompt("Enter number")
+
+// if (time>=0 && time<1200) {
+//     document.write("Good Morning!")
+    
+// }
+// else if (time>=1200 && time<1700) {
+//     document.write("Good afternoon!")
+
+
+    
+// }
+// else if (time>=1700 && time<2100) {
+//     document.write("Good evening!")
+
+
+    
+// }
+// else if (time>=2100 && time<2359) {
+//     document.write("Good night!")
+
+
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//                  Ch#14-16
+
+//Q1
+// var citie = [];
+
+
+//Q2
+// var ci = {}
+
+// //Q3
+//  var citie = ["karachi","Lahore"]
+
+//  //Q4
+
+//  var num= [1,2,3]
+
+//  //Q5
+
+//  var boolenn = [false,true,true]
+
+ //Q6
+
+// var arr = ["karachi", 5,'Lahore',5]
+
+//Q7
+// var degree = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil", "PhD"]
+
+// for (let index = 0; index < degree.length; index++) {
+//     document.write(index+1,") ",degree[index],"<br>")
+
+// }
+
+
+//Q8
+
+// var td = []
+// var tdMarks = []
+// for (let index = 0; index < 3; index++) {
+//     td[index]=prompt("Enter "+ parseInt(index+1)+" Student name")
+//     tdMarks[index] = prompt("Enter "+td[index]+"'s Marks ")
+// }
+// for (let index = 0; index < 3; index++) {
+//     document.write("Score of "+td[index]+" is "+(td[index]/500)*100)
+
+// }
+
+//Q10
+
+// var num = [320,230,480,120]
+// num = num.sort()
+// document.write(num)
+
+//Q11
+// var cities = ["Karachi","Lahore","Quetta","Islambad","Peshawar"]
+// var selectcities = [cities[0],citie[1]]
+
+
+// Q12
+
+// var na = ["this","is","my","cat"];
+
+// var naa = na.join(' ');
+
+//Q13
+
+// var va = ["Keyboard","Mouse","printer","monitor"]
+// for (let index = 0; index < 4; index++) {
+//     document.write(va.shift(),"<br>")
+
+
+// }
+
+//Q14
+// var va = ["Keyboard","Mouse","printer","monitor"]
+// for (let index = 0; index < 4; index++) {
+//     document.write(va.pop(),"<br>")
+
+
+// }
+
+//Q15
+
+// var va = ["Apple", "Samsung", "Motorola", "Nokia", "Sony" ,"Haier"]
+// document.write(va)
+
+
+
+
+
+
+
+
+
+
